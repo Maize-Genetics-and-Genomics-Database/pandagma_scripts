2c6ebd021f57a31f50d76da9c17114d0dcc7a93d
E.Cannon <ekcannon@iastsate.edu>
2023-10-11 17:02:04 -0500

    Initial import

--- helpers/format_pandagma_table.sh
+++ helpers/format_pandagma_table.sh
@@ -2,72 +2,28 @@
 
 # file: format_pandagma_table.sh
 # 
-# Purpose: Format _genome.txt files for R table.
-#          This also puts multiple transcripts per genome per pan-gene 
-#          in a single cell separated by semicolons.
+# Purpose: Format _genome.txt files for loading into MaizeGDB.
 #
-# DEPENDENCIES:
-#   Imported here: R, bedtools
-#   Must be installed: R tidyverse via install.packages("tidyverse")
-#                          (may complain about missing 'ragg': ignore)
+# Produces a table of pan-IDs and annotation IDs and chromosomes for each constituent gene:
+#   panID gene-model chr start end strand panChr-full panChr pan-start pan-end pan-strand exemplar
+# Example:
+#   pan00001 Zd00001aa032704_T001 Zd-Gigi_PanAnd-1.chr9 48262170 48264952 + Zea.pan3.chr09_039400_pan00001 chr09 039400 3940000 3941000 + Zm00001eb376300_T001
 #
 # NOTE: Run this script from within the pandagma output file
 #
 # History:
-#  2023     MW    created
-#  10/05/23 eksc  consolidated 
+#  10/01/23  sbc  created
+#  10/10/23 eksc  adapted
 
-PREFIX=$1
+# From 23_syn_pan_pctl25_posn_cds.fna, extract table of IDs and positions
+grep '>' 23_syn_pan_pctl25_posn_cds.fna | 
+  perl -pe 's/^>//; s/ /\t/g; s/^(\w+\.\w+\.([^_]+)_(\d+)_(\w+))/$1\t$2\t$3\t$4/' |
+  sort -k4,4 > 23_syn_pan_pctl25_posn.tsv
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-echo "Script directory: $SCRIPT_DIR"
-
-# Load modules
-ml r/4.3.0
-ml bedtools
-
-# To hold temp working files
-mkdir pandagma_csv
-
-
-# Split 22_syn_pan_aug_extra_pctl25_posn.hsh.tsv into files named *.genome.txt 
-#   panID transcript prefix.chromosome start end strand
-#       --> panID transcript chromosome start end 
-#   without transcript suffix
-cat 22_syn_pan_aug_extra_pctl25_posn.hsh.tsv | tr '.' '\t' | 
-  awk -v OFS="\t" '{print $3"_genome.txt",$1,$2,$3,$4}' | sort -k1,1 | uniq | awk '{print>"pandagma_csv/"$1}'
-
-
-# Extract pan gene chromosomes from 23_syn_pan_pctl25_posn_cds.fna
-cat 23_syn_pan_pctl25_posn_cds.fna | 
-  perl -nle 'if(/>.*\.(chr.*?)_\d+_(pan\d+)/){print "$2\t$1"}' | sort -k1,1n > pandagma_csv/pan_chr.txt
-
-
-cd pandagma_csv
-
-# Make the pan-gene membership table
-for sample in *_genome.txt
-  do
-   describer=$(echo ${sample} | sed 's/_genome.txt//')
-   echo $sample $describer
-
-   cat ${sample} | cut -f 2,3,5 | groupBy -g 1 -c 2,3 -o distinct,distinct | tr ',' ';' | 
-     awk -v x=${describer} 'BEGIN { OFS=","; print "panID", "transcripts_"x,"chromosomeIDs_"x} {print $0}' | 
-     sed -e 's/-1_genome.txt//g; s/-2_genome.txt//g; s/-3_genome.txt//g; s/-4_genome.txt//g' | 
-     tr '\t' ',' > ${describer}_pandagma.csv
-
-done
-rm *_genome.txt
-
+# Join to table 22_syn_pan_aug_extra_pctl25_posn.hsh.tsv
+join -1 1 -2 4 -a 1 22_syn_pan_aug_extra_pctl25_posn.hsh.tsv 23_syn_pan_pctl25_posn.tsv | 
+  perl -pe 's/ /\t/g' > 22_syn_pan_aug_extra_pctl25_posn.hsh.extended.tsv
+ 
 echo
-echo "Run R script format_pandagma_table.R to create pandagma_table.txt, adding chrs to members"
-Rscript $SCRIPT_IDR/format_pandagma_table.R
-
-
-# Add the pan-gene prefix and chr to the table
-cat pandagma_table.txt | head -n -1 | sort -k 1,1n |
-  join -a 1 - pandagma_csv/pan_chr.txt |
-  perl -nle 'if(/(.*?)\s(.*)pan_(chr\d+)/){print "$1\t$3\t$2"}else{s/(.*?)\s/$1\tNA\t/;print}' |
-  awk -v PRE=$PREFIX '{print PRE $0}' > pandagma_table_chr.txt
-
-:
+echo "Table to load: 22_syn_pan_aug_extra_pctl25_posn.hsh.extended.tsv"
+echo
